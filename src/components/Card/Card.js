006a520dbd0d00b572da29de141472daed2d73a4
Vu Dao <vu.dao@aminocom.com>
2019-02-13 17:53:04 +0700

    style: Use prettier to format code chore: Add precommit hooks for prettier

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import cardBack from '../../static/images/Back.png';
+import React, { Component } from "react";
+import cardBack from "../../static/images/Back.png";
 
 class Card extends Component {
-    render() {
-        const { isShown, name, image, description } = this.props;
-        if (isShown === true) {
-          return (
-            <div className="card-holder">
-              <img className="cards" alt={name} src={image}></img>
-              <div className="description">
-                <div className="description name">{name}</div>
-                <div className="description info">{description}</div>
-              </div>
-            </div>
-          );
-        }
-        return (
-          <div className="card-holder">
-            <img className="cards card-back" alt="Card Back" src={cardBack}></img>
+  render() {
+    const { isShown, name, image, description } = this.props;
+    if (isShown === true) {
+      return (
+        <div className="card-holder">
+          <img className="cards" alt={name} src={image} />
+          <div className="description">
+            <div className="description name">{name}</div>
+            <div className="description info">{description}</div>
           </div>
-        );
+        </div>
+      );
     }
+    return (
+      <div className="card-holder">
+        <img className="cards card-back" alt="Card Back" src={cardBack} />
+      </div>
+    );
+  }
 }
 
 export default Card;
