9ba6a9d58706c85839cd47453c832c7dd35a52b5
Quan Dao <daq30899@gmail.com>
2019-04-21 17:50:24 +0300

    Updates

---
+++ precache-manifest.57071a59d119c3645c364e581827c6d4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b0115d0a71a4e03f413eb32c82c160b",
+    "url": "/React-Coup-Online/static/media/Contessa.6b0115d0.png"
+  },
+  {
+    "revision": "2a26ef5658bc8bc29f89",
+    "url": "/React-Coup-Online/static/css/main.093cff9f.chunk.css"
+  },
+  {
+    "revision": "c9671e61420cddf41cee",
+    "url": "/React-Coup-Online/static/js/runtime~main.2b272bd7.js"
+  },
+  {
+    "revision": "409636afdb6842fe1ec8",
+    "url": "/React-Coup-Online/static/js/2.596b1d1f.chunk.js"
+  },
+  {
+    "revision": "fb0ceeed862fd1103dbdbfe25ca9e254",
+    "url": "/React-Coup-Online/static/media/Duke.fb0ceeed.png"
+  },
+  {
+    "revision": "2a26ef5658bc8bc29f89",
+    "url": "/React-Coup-Online/static/js/main.545bc552.chunk.js"
+  },
+  {
+    "revision": "6cc57e5c136f27f5a58783297ca0b551",
+    "url": "/React-Coup-Online/static/media/Captain.6cc57e5c.png"
+  },
+  {
+    "revision": "3c08f807c52f215509a67675be1a934c",
+    "url": "/React-Coup-Online/static/media/Assassin.3c08f807.png"
+  },
+  {
+    "revision": "e1d61479ba185e5ec81eb1e6b3719727",
+    "url": "/React-Coup-Online/static/media/Ambassador.e1d61479.png"
+  },
+  {
+    "revision": "c80ea36933eb6dc0cb461a50d4aa7f75",
+    "url": "/React-Coup-Online/static/media/Back.c80ea369.png"
+  },
+  {
+    "revision": "649e54633a5924c34f22398702cb98e7",
+    "url": "/React-Coup-Online/index.html"
+  }
+];
